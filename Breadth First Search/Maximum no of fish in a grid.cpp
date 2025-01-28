4f7f084ba5736abcff1dbd1a81f58754e6b5bbfe
1-Ujjwal <kingayushman7@gmail.com>
2025-01-28 15:38:19 +0530

    Leetcode problem using BFS

---
+++ Breadth First Search/Maximum no of fish in a grid.cpp
@@ -0,0 +1,53 @@
+// Leetcode problem no 2658
+
+class Solution
+{
+public:
+    vector<vector<int>> directions = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+    int m, n;
+    int bfs(int i, int j, vector<vector<int>> &grid)
+    {
+        queue<pair<int, int>> que;
+        que.push({i, j});
+        int fishcount = grid[i][j];
+        grid[i][j] = 0;
+
+        while (!que.empty())
+        {
+            i = que.front().first;
+            j = que.front().second;
+            que.pop();
+
+            for (auto &dir : directions)
+            {
+                int i_ = dir[0] + i;
+                int j_ = dir[1] + j;
+                if (i_ >= 0 && j_ >= 0 && i_ < m && j_ < n && grid[i_][j_] > 0)
+                {
+                    que.push({i_, j_});
+                    fishcount += grid[i_][j_];
+                    grid[i_][j_] = 0;
+                }
+            }
+        }
+        return fishcount;
+    }
+
+    int findMaxFish(vector<vector<int>> &grid)
+    {
+        m = grid.size();
+        n = grid[0].size();
+        int maxfish = 0;
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (grid[i][j] > 0)
+                {
+                    maxfish = max(maxfish, bfs(i, j, grid));
+                }
+            }
+        }
+        return maxfish;
+    }
+};
